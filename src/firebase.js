fa2ad79c5feddce88a97ae725ff3b6c8e09f32d5
michaelgibbons22 <michaeloradell@gmail.com>
2025-07-25 00:15:00 -0400

    Trigger Firebase deploy workflow

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcbKjuJ-QeeGI3xmYUsnnAq0VQUP_w-LY",
+  authDomain: "a4a-website-41c5d.firebaseapp.com",
+  projectId: "a4a-website-41c5d",
+  storageBucket: "a4a-website-41c5d.firebasestorage.app",
+  messagingSenderId: "1095772880707",
+  appId: "1:1095772880707:web:9a3489c094c7be421fb9f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
